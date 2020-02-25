ba47ecd9590867197420d2c85fd8b0f817570418
YRPzfte <1243016396@qq.com>
2020-02-25 11:28:34 +0800

    20200225-112834

--- our_stitching.cpp
+++ our_stitching.cpp
@@ -68,143 +68,148 @@ int main (int argc, char** argv) {
     construct_sift_point_cloud(source_depth, target_depth, source_sift_cloud, target_sift_cloud);
     Eigen::Matrix4f transform = calculate_stitch_transformation(source_sift_cloud, target_sift_cloud);
 
-//    //拼接结果展示
-//    transformPointCloud(*source_cloud, *source_cloud, transform);
-//    *source_cloud += *target_cloud;
-//    show_cloud(source_cloud);
-
-    timer.end("[完成3D场景拼接]");
-    //最佳视点区域分割
-    Eigen::Matrix4f source_transform = transform;
-    source_transform(0, 3) = 0;
-    source_transform(1, 3) = 0.5 * transform(1, 3);
-    source_transform(2, 3) = 0;
-    transformPointCloud(*source_cloud, *source_cloud, source_transform);
-    Mat source_coordinate_img = get_coordinate_image(source_cloud);
-
-    Eigen::Matrix4f target_transform = Eigen::Matrix4f::Identity();
-    target_transform(1, 3) = -0.5 * transform(1, 3);
-    transformPointCloud(*target_cloud, *target_cloud, target_transform);
-    Mat target_coordinate_img = get_coordinate_image(target_cloud);
-
-//    Mat source_view_img = draw_cloud_image(source_cloud);
-//    Mat target_view_img = draw_cloud_image(target_cloud);
-//    imwrite("source_view_img.png", source_view_img);
-//    imwrite("target_view_img.png", target_view_img);
-
-    float x_offset = transform(0, 3);
-    float z_offset = transform(2, 3);
-
-    float source_min_col, target_max_col;
-    calculate_cloud_overlap_edge(source_coordinate_img, target_coordinate_img, source_min_col, target_max_col, x_offset, z_offset);
-    int source_left_col = 850;//int(source_min_col);
-    int target_left_col = 800;//int(target_max_col);
-
-    for (int i = 0 ; i < max_size ; ++i) {
-        for (int j = 0 ; j < max_size ; ++j) {
-            record[i][j] = -1;
-        }
+    pcl::visualization::CloudViewer viewer ("Viewer");
+    viewer.showCloud (source_cloud); //最后展示左视角+右视角点云图 
+    while (!viewer.wasStopped ())
+    {
     }
-    //计算左视点分割结果
-    findBestInterval(source_coordinate_img, target_coordinate_img, source_left_col, target_left_col, x_offset, z_offset, -max_size, max_size);
+    //3D场景重建完成并且用vtk显示
 
-    //计算对应的右视点分割结果
-    int source_right_col = source_left_col;
-    int target_right_col = target_left_col;
-    calculate_according_right_edge(source_coordinate_img, target_coordinate_img, source_right_col, target_right_col);
-
-
-    std::cout << source_left_col << "--" << source_right_col << " | " << target_left_col << "--" << target_right_col << std::endl;
-    std::cout << "overlap outer width:" << max_distance[source_left_col][target_left_col] << " overlap inner width:" << min_distance[source_left_col][target_left_col] << " overlap_rate:" << record[source_left_col][target_left_col] << std::endl;
-    timer.end("[完成最佳视点区域分割]");
-
-
-    //逆映射
-    //计算左右视图中视点迁移区域宽度
-    int left_overlap_width, right_overlap_width;
-    calculate_overlap_width(source_left_col, target_left_col, source_right_col, target_right_col, left_overlap_width, right_overlap_width);
-    std::cout << "Left overlap width:" << left_overlap_width << " | " << "Right overlap width:" << right_overlap_width << std::endl;
-
-//    show_split_view(source_cloud, target_cloud, source_transform, target_transform, source_right_col, target_right_col, x_offset, z_offset);
-
-    //---左视图
-    //---source image
-    vector<Point2> source_left_origin_points, source_left_dest_points;
-    Mat source_left_img = draw_cloud_image(source_cloud, source_left_points, source_left_origin_points, source_left_dest_points, 0, source_left_col);
-
-
-    //---target image
-    vector<Point2> target_left_origin_points, target_left_dest_points;
-    Mat target_left_img = draw_cloud_image(target_cloud, target_left_points, target_left_origin_points, target_left_dest_points, target_left_col, img_width * extern_rate);
-
-    //---overlap image
-    vector<Point2> view_source_origin_points, view_source_dest_points;
-    Mat source_left_overlap_img = Mat::zeros(img_height * extern_rate, left_overlap_width, CV_8UC3);
-    Mat target_left_overlap_img = Mat::zeros(img_height * extern_rate, left_overlap_width, CV_8UC3);
-
-    append_overlap_image(source_cloud, target_cloud, x_offset, z_offset, source_left_col, target_left_col, left_overlap_width,
-                                               source_left_points, source_left_origin_points, source_left_dest_points,
-                                               target_left_points, target_left_origin_points, target_left_dest_points,
-                                               source_left_overlap_img, target_left_overlap_img);
-    Consistent_Stitching left_consist(multi_left_images);
-    left_consist.setSize(img_width * extern_rate, img_height * extern_rate);
-    vector<pair<Point2, Point2> > source_left_mapping_pairs = join_pairs(source_left_origin_points, source_left_dest_points);
-    vector<pair<Point2, Point2> > target_left_mapping_pairs = join_pairs(target_left_origin_points, target_left_dest_points);
-    left_consist.set_mapping_pair(source_left_mapping_pairs, target_left_mapping_pairs);
-    vector<vector<pair<Point2, Point2> > > left_boundary_pairs = left_consist.calulate_boundary_pair();
-    left_consist.set_boundary_pair(left_boundary_pairs);
-    left_consist.setWeightToAlignmentTerm(1);
-    left_consist.setWeightToSimilarityTerm(0.75, GLOBAL_ROTATION_3D_METHOD);
-
-    Mat source_left_view, target_left_view;
-    Mat stitch_left_img = left_consist.applyWarping(BLEND_AVERAGE, left_overlap_width, source_left_col, target_left_col);
-
-
-//    imwrite("stitching_left.png", stitch_left_img);
-    timer.end("[完成左视图的拼接]");
-//    return 0;
-    //---右视图
-    Eigen::Matrix4f left2right_transform = Eigen::Matrix4f::Identity();
-    left2right_transform(0, 3) = -0.12001928710937500000;
-    transformPointCloud(*source_cloud, *source_cloud, left2right_transform);
-    transformPointCloud(*target_cloud, *target_cloud, left2right_transform);
-
-    //---source image
-    vector<Point2> source_right_origin_points, source_right_dest_points;
-    Mat source_right_img = draw_cloud_image(source_cloud, source_right_points, source_right_origin_points, source_right_dest_points, 0, source_right_col);
-
-    //---target image
-    vector<Point2> target_right_origin_points, target_right_dest_points;
-    Mat target_right_img = draw_cloud_image(target_cloud, target_right_points, target_right_origin_points, target_right_dest_points, target_right_col, img_width * extern_rate);
-
-    //---overlap image
-    Mat source_right_overlap_img = Mat::zeros(img_height * extern_rate, right_overlap_width, CV_8UC3);
-    Mat target_right_overlap_img = Mat::zeros(img_height * extern_rate, right_overlap_width, CV_8UC3);
-    append_overlap_image(source_cloud, target_cloud, x_offset, z_offset, source_right_col, target_right_col, right_overlap_width,
-                                               source_right_points, source_right_origin_points, source_right_dest_points,
-                                               target_right_points, target_right_origin_points, target_right_dest_points,
-                                               source_right_overlap_img, target_right_overlap_img);
-    MultiImages multi_rigth_images(source_dir, target_dir, "right", LINES_FILTER_WIDTH, LINES_FILTER_LENGTH);
-
-    Consistent_Stitching right_consist(multi_rigth_images);
-    right_consist.setPaths(left_consist.getMappingPaths());
-    right_consist.setSize(img_width * extern_rate, img_height * extern_rate);
-    vector<pair<Point2, Point2> > source_right_mapping_pairs = join_pairs(source_right_origin_points, source_right_dest_points);
-    vector<pair<Point2, Point2> > target_right_mapping_pairs = join_pairs(target_right_origin_points, target_right_dest_points);
-    right_consist.set_mapping_pair(source_right_mapping_pairs, target_right_mapping_pairs);
-    vector<vector<pair<Point2, Point2> > > right_boundary_pairs = right_consist.calulate_boundary_pair();
-
-    right_consist.set_boundary_pair(right_boundary_pairs);
-    right_consist.setWeightToAlignmentTerm(1);
-    right_consist.setWeightToSimilarityTerm(0.75, GLOBAL_ROTATION_3D_METHOD);
-
-    Mat stitch_right_img = right_consist.applyWarping(BLEND_AVERAGE, right_overlap_width, source_right_col, target_right_col);
-//    imwrite("stitching_right.png", stitch_right_img);
-    timer.end("[完成右视图的拼接]");
-
-    crop_stitching_result(stitch_left_img, stitch_right_img);
-    imwrite("our_left.png", stitch_left_img);
-    imwrite("our_right.png", stitch_right_img);
-    timer.end("[完成视图的裁剪]");
 
 }
+
+//     timer.end("[完成3D场景拼接]");
+//     //最佳视点区域分割
+//     Eigen::Matrix4f source_transform = transform;
+//     source_transform(0, 3) = 0;
+//     source_transform(1, 3) = 0.5 * transform(1, 3);
+//     source_transform(2, 3) = 0;
+//     transformPointCloud(*source_cloud, *source_cloud, source_transform);
+//     Mat source_coordinate_img = get_coordinate_image(source_cloud);
+
+//     Eigen::Matrix4f target_transform = Eigen::Matrix4f::Identity();
+//     target_transform(1, 3) = -0.5 * transform(1, 3);
+//     transformPointCloud(*target_cloud, *target_cloud, target_transform);
+//     Mat target_coordinate_img = get_coordinate_image(target_cloud);
+
+// //    Mat source_view_img = draw_cloud_image(source_cloud);
+// //    Mat target_view_img = draw_cloud_image(target_cloud);
+// //    imwrite("source_view_img.png", source_view_img);
+// //    imwrite("target_view_img.png", target_view_img);
+
+//     float x_offset = transform(0, 3);
+//     float z_offset = transform(2, 3);
+
+//     float source_min_col, target_max_col;
+//     calculate_cloud_overlap_edge(source_coordinate_img, target_coordinate_img, source_min_col, target_max_col, x_offset, z_offset);
+//     int source_left_col = 850;//int(source_min_col);
+//     int target_left_col = 800;//int(target_max_col);
+
+//     for (int i = 0 ; i < max_size ; ++i) {
+//         for (int j = 0 ; j < max_size ; ++j) {
+//             record[i][j] = -1;
+//         }
+//     }
+//     //计算左视点分割结果
+//     findBestInterval(source_coordinate_img, target_coordinate_img, source_left_col, target_left_col, x_offset, z_offset, -max_size, max_size);
+
+//     //计算对应的右视点分割结果
+//     int source_right_col = source_left_col;
+//     int target_right_col = target_left_col;
+//     calculate_according_right_edge(source_coordinate_img, target_coordinate_img, source_right_col, target_right_col);
+
+
+//     std::cout << source_left_col << "--" << source_right_col << " | " << target_left_col << "--" << target_right_col << std::endl;
+//     std::cout << "overlap outer width:" << max_distance[source_left_col][target_left_col] << " overlap inner width:" << min_distance[source_left_col][target_left_col] << " overlap_rate:" << record[source_left_col][target_left_col] << std::endl;
+//     timer.end("[完成最佳视点区域分割]");
+
+
+//     //逆映射
+//     //计算左右视图中视点迁移区域宽度
+//     int left_overlap_width, right_overlap_width;
+//     calculate_overlap_width(source_left_col, target_left_col, source_right_col, target_right_col, left_overlap_width, right_overlap_width);
+//     std::cout << "Left overlap width:" << left_overlap_width << " | " << "Right overlap width:" << right_overlap_width << std::endl;
+
+// //    show_split_view(source_cloud, target_cloud, source_transform, target_transform, source_right_col, target_right_col, x_offset, z_offset);
+
+//     //---左视图
+//     //---source image
+//     vector<Point2> source_left_origin_points, source_left_dest_points;
+//     Mat source_left_img = draw_cloud_image(source_cloud, source_left_points, source_left_origin_points, source_left_dest_points, 0, source_left_col);
+
+
+//     //---target image
+//     vector<Point2> target_left_origin_points, target_left_dest_points;
+//     Mat target_left_img = draw_cloud_image(target_cloud, target_left_points, target_left_origin_points, target_left_dest_points, target_left_col, img_width * extern_rate);
+
+//     //---overlap image
+//     vector<Point2> view_source_origin_points, view_source_dest_points;
+//     Mat source_left_overlap_img = Mat::zeros(img_height * extern_rate, left_overlap_width, CV_8UC3);
+//     Mat target_left_overlap_img = Mat::zeros(img_height * extern_rate, left_overlap_width, CV_8UC3);
+
+//     append_overlap_image(source_cloud, target_cloud, x_offset, z_offset, source_left_col, target_left_col, left_overlap_width,
+//                                                source_left_points, source_left_origin_points, source_left_dest_points,
+//                                                target_left_points, target_left_origin_points, target_left_dest_points,
+//                                                source_left_overlap_img, target_left_overlap_img);
+//     Consistent_Stitching left_consist(multi_left_images);
+//     left_consist.setSize(img_width * extern_rate, img_height * extern_rate);
+//     vector<pair<Point2, Point2> > source_left_mapping_pairs = join_pairs(source_left_origin_points, source_left_dest_points);
+//     vector<pair<Point2, Point2> > target_left_mapping_pairs = join_pairs(target_left_origin_points, target_left_dest_points);
+//     left_consist.set_mapping_pair(source_left_mapping_pairs, target_left_mapping_pairs);
+//     vector<vector<pair<Point2, Point2> > > left_boundary_pairs = left_consist.calulate_boundary_pair();
+//     left_consist.set_boundary_pair(left_boundary_pairs);
+//     left_consist.setWeightToAlignmentTerm(1);
+//     left_consist.setWeightToSimilarityTerm(0.75, GLOBAL_ROTATION_3D_METHOD);
+
+//     Mat source_left_view, target_left_view;
+//     Mat stitch_left_img = left_consist.applyWarping(BLEND_AVERAGE, left_overlap_width, source_left_col, target_left_col);
+
+
+// //    imwrite("stitching_left.png", stitch_left_img);
+//     timer.end("[完成左视图的拼接]");
+// //    return 0;
+//     //---右视图
+//     Eigen::Matrix4f left2right_transform = Eigen::Matrix4f::Identity();
+//     left2right_transform(0, 3) = -0.12001928710937500000;
+//     transformPointCloud(*source_cloud, *source_cloud, left2right_transform);
+//     transformPointCloud(*target_cloud, *target_cloud, left2right_transform);
+
+//     //---source image
+//     vector<Point2> source_right_origin_points, source_right_dest_points;
+//     Mat source_right_img = draw_cloud_image(source_cloud, source_right_points, source_right_origin_points, source_right_dest_points, 0, source_right_col);
+
+//     //---target image
+//     vector<Point2> target_right_origin_points, target_right_dest_points;
+//     Mat target_right_img = draw_cloud_image(target_cloud, target_right_points, target_right_origin_points, target_right_dest_points, target_right_col, img_width * extern_rate);
+
+//     //---overlap image
+//     Mat source_right_overlap_img = Mat::zeros(img_height * extern_rate, right_overlap_width, CV_8UC3);
+//     Mat target_right_overlap_img = Mat::zeros(img_height * extern_rate, right_overlap_width, CV_8UC3);
+//     append_overlap_image(source_cloud, target_cloud, x_offset, z_offset, source_right_col, target_right_col, right_overlap_width,
+//                                                source_right_points, source_right_origin_points, source_right_dest_points,
+//                                                target_right_points, target_right_origin_points, target_right_dest_points,
+//                                                source_right_overlap_img, target_right_overlap_img);
+//     MultiImages multi_rigth_images(source_dir, target_dir, "right", LINES_FILTER_WIDTH, LINES_FILTER_LENGTH);
+
+//     Consistent_Stitching right_consist(multi_rigth_images);
+//     right_consist.setPaths(left_consist.getMappingPaths());
+//     right_consist.setSize(img_width * extern_rate, img_height * extern_rate);
+//     vector<pair<Point2, Point2> > source_right_mapping_pairs = join_pairs(source_right_origin_points, source_right_dest_points);
+//     vector<pair<Point2, Point2> > target_right_mapping_pairs = join_pairs(target_right_origin_points, target_right_dest_points);
+//     right_consist.set_mapping_pair(source_right_mapping_pairs, target_right_mapping_pairs);
+//     vector<vector<pair<Point2, Point2> > > right_boundary_pairs = right_consist.calulate_boundary_pair();
+
+//     right_consist.set_boundary_pair(right_boundary_pairs);
+//     right_consist.setWeightToAlignmentTerm(1);
+//     right_consist.setWeightToSimilarityTerm(0.75, GLOBAL_ROTATION_3D_METHOD);
+
+//     Mat stitch_right_img = right_consist.applyWarping(BLEND_AVERAGE, right_overlap_width, source_right_col, target_right_col);
+// //    imwrite("stitching_right.png", stitch_right_img);
+//     timer.end("[完成右视图的拼接]");
+
+//     crop_stitching_result(stitch_left_img, stitch_right_img);
+//     imwrite("our_left.png", stitch_left_img);
+//     imwrite("our_right.png", stitch_right_img);
+//     timer.end("[完成视图的裁剪]");
+
+// }
